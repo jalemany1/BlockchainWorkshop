500d7eedc9068fbe583f4cf34011df4f6566f0b8
Alejandro Marco <alejandromarco@MacBook-Pro-de-Alejandro.local>
2018-10-30 14:48:01 +0000

    New method added plus more ideas

--- smart-contract/eventh.sol
+++ smart-contract/eventh.sol
@@ -17,12 +17,16 @@ contract eventh {
 
     address public organiser; //Identifing the owner a.k. as the organizer
     string public eventName; //Event name, could be changed for an ID
-    uint256 public value; //Value per ticket
+    uint256 public value; //We will use value to define the price of the ticket ADJUST!
     bool public eventFree; //Set if the event is free or not
     uint8 public totalTickets; //total number of tickets
     uint8 public issuedTickets; //Tickets sold
     mapping(address => Ticket) assistants; //List of assistants
-    //Ticket[] ticketStack;
+
+    ///WE NEED A EVENT DATE VARIABLE TO MAKE THIS CONTRACT WORK BETTER!!!!!!!
+
+    //Contract creators addresses
+    address public creatorAM;
 
                         /// ----- MODIFIERS ----- ///
 
@@ -37,12 +41,16 @@ contract eventh {
 
         //require(msg.value == 1 ether);
 
-        organiser = msg.sender; //The organiser is the one that creates the contract
+         //The organiser is the one that creates the contract
+        organiser = msg.sender;
         eventName = _eventName;
         value = _value;
         eventFree = _eventFree;
         totalTickets = _totalTickets;
         issuedTickets = 0;
+
+        //Reference creator:
+        creatorAM = 0xeCdBEEf1027bD42983c35DB6F5feaEC1B7522FaE;
     }//constructor
 
 
@@ -69,14 +77,48 @@ contract eventh {
         }else{
             assistant.transfer(1 ether);
         }//if not inform the user that already has a ticketand return ether
-
     }//takeTicket
 
                     /// ----- ORGANISER CHECKS ATTENDANCE ----- ///
 
+    //It would be nice to only let the organoiser do that the day of the event
     function isHere(address _assistant) onlyOrganiser {
 
+        //After chechk that the address has a valid ticket and has not been punched:
+         if(assistants[_assistant].tSold == true && assistants[_assistant].tPunched == false) {
+             assistants[_assistant].tPunched = true; //Punch the ticket
+             if(eventFree == true){
+                _assistant.transfer(1 ether); //return the ether deposit to the assistant
+             }//just if the event is free
+         }
     }//isHere
 
+                     /// ----- EVENT HAS END ----- ///
 
-}
+    //The idea here is to end with the contract after a few days of the event date.
+    //Then, the ethers that are in the conctract, due to people that has not assisted
+    //will be send to the organizer account and the creator of the contract as a gesture
+    //of greeting for the creation of this contract. The split will change if we are on
+    //a free or a paid event. It could be something like:
+    //Free event: 50% for creators
+    //Paid event: 5% for creators
+
+
+                     /// ----- QUALITY CONTROL ----- ///
+
+    //Assistant can leave a puntuation after the workshop, if a significant numbers
+    //of reviews indicate that something is wrong some things can change.
+
+    
+                /// ----- CANCEL THE EVENT BY ORGANIZER ----- ///
+
+    //If the event is cancelled the organizer can execute this function that will
+    //refund the ether to the assistants.
+
+                 /// -----     GET REFUND BY USER    ----- ///
+
+    //We can make the event refundable letting the user cancel the ticket X days
+    //before the event date.
+
+
+}//
